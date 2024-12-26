36ea370d150044ebe949132c3cb01bb974a9a66e
theharsh13 <harshvpatel24@gmail.com>
2024-12-26 19:03:02 +0530

    334-increasing-triplet-sub-sequence

---
+++ src/leetCode75/IncreasingTripletSubSeq.java
@@ -0,0 +1,51 @@
+package leetCode75;
+
+import core.Solution;
+
+/*
+* 334. https://leetcode.com/problems/increasing-triplet-subsequence
+*
+* Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+
+Example 1:
+Input: nums = [1,2,3,4,5]
+Output: true
+Explanation: Any triplet where i < j < k is valid.
+*
+Example 2:
+Input: nums = [5,4,3,2,1]
+Output: false
+Explanation: No triplet exists.
+*
+Example 3:
+Input: nums = [2,1,5,0,4,6]
+Output: true
+Explanation: The triplet (3, 4, 5) is valid because nums[3] == 0 < nums[4] == 4 < nums[5] == 6.
+
+Constraints:
+1 <= nums.length <= 5 * 105
+-231 <= nums[i] <= 231 - 1
+
+Follow up: Could you implement a solution that runs in O(n) time complexity and O(1) space complexity?
+* */
+
+public class IncreasingTripletSubSeq implements Solution {
+
+    public boolean increasingTriplet(int[] nums) {
+        int min1 = 232;
+        int min2 = Integer.MAX_VALUE;
+        for(int n : nums) {
+            if(n <= min1) min1 = n;
+            else if(n <= min2) min2 = n;
+            else return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void runTestCases() {
+        System.out.println("Result: " + increasingTriplet(new int[]{1,2,3,4,5}));
+        System.out.println("Result: " + increasingTriplet(new int[]{5,4,3,2,1}));
+        System.out.println("Result: " + increasingTriplet(new int[]{2,1,5,0,4,6}));
+    }
+}
